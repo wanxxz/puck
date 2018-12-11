5a9b1ba8fde8dab65a986b4cb0c12b246cae1229
Yiwan Zhou <mail@yiwan.me>
2018-12-11 20:24:56 +0800

    refactor: template

--- src/me/yiwan/puck/template.clj
+++ src/me/yiwan/puck/template.clj
@@ -10,10 +10,6 @@
 ;; each time a resource or file used by a template/snippet is updated the namespace is reloaded 
 (enlive-reload/auto-reload *ns*)
 
-(def path (.getPath (io/file (:wd conf) (-> conf :dir :template))))
-
-(def pattern #".*\.html$")
-
 (defmacro generate-template-function
   [name file]
   `(intern
@@ -25,8 +21,9 @@
 
 (defn find-template-files
   []
-  (mapv
-   (fn [file] [(fs/base-name file true) file])
-   (fs/find-files path pattern)))
+  (let [path (.getPath (io/file (:wd conf) (-> conf :dir :template))) pattern #".*\.html$"]
+    (mapv
+     (fn [file] [(fs/base-name file true) file])
+     (fs/find-files path pattern))))
 
 (defstate template :start (doseq [[n f] (find-template-files)] (generate-template-function n f)))
