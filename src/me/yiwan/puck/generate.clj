d3b7fb4d4b84fabc1d1a3d4f0f36b3fb8f41522f
Yiwan Zhou <mail@yiwan.me>
2018-12-13 10:48:38 +0800

    feat: generate

---
+++ src/me/yiwan/puck/generate.clj
@@ -0,0 +1,33 @@
+(ns me.yiwan.puck.generate
+  (:require [clojure.java.io :as io]
+            [me.raynes.fs :as fs]
+            [mount.core :refer [defstate]]
+            [me.yiwan.puck.conf :refer [conf]]
+            [me.yiwan.puck.content :refer [generate-html]]))
+
+(defn find-files
+  [dir pattern]
+  (let [path (.getPath (io/file (:wd conf) (-> conf :dir dir)))]
+       (fs/find-files path pattern)))
+
+(defn find-output-file
+  [dir name ext]
+  (let [output-file (io/file
+                     (:wd conf)
+                     (-> conf :dir :root)
+                     (-> conf :dir dir)
+                     (str name ".html"))]
+    (if (-> output-file fs/file? not)
+      (.createNewFile output-file))
+    output-file))
+
+(defn generate-content
+  [dir]
+  (doseq [input-file (find-files dir #".*\.md$")] 
+    (let [input-file-name (fs/base-name input-file true)
+          output-file (find-output-file dir input-file-name ".html")]
+      (spit output-file (generate-html (slurp input-file))))))
+
+(defstate generate :start (do
+                            (generate-content :post)
+                            (generate-content :page)))
