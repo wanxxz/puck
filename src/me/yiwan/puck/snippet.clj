856a45360948cca41060afe7e5fa5afb397d29b0
Yiwan Zhou <mail@yiwan.me>
2018-12-19 15:38:54 +0800

    refactor: preset snippets

--- src/me/yiwan/puck/snippet.clj
+++ src/me/yiwan/puck/snippet.clj
@@ -3,33 +3,31 @@
             [me.raynes.fs :as fs]
             [me.yiwan.puck.conf :refer [conf]]
             [me.yiwan.puck.helper :refer [list-post]]
-            [mount.core :refer [defstate]]
+            [mount.core :refer [defstate args]]
             [net.cgrand.enlive-html :as enlive]
             net.cgrand.reload))
 
-(net.cgrand.reload/auto-reload *ns*)
+;; (net.cgrand.reload/auto-reload *ns*)
 
 (def prefix "snippet-")
 
-(def preset #{"head" "post-list"}) 
+(def preset #{"head" "post-list"})
 
-(enlive/defsnippet snippet-post-list
-  (io/file (:wd conf) (-> conf :dir :snippet) "post-list.html")
-  [#{:ul :ol}]
-  [& args]
-  [#{:ul :ol} [:li enlive/first-of-type]] (enlive/clone-for [post (list-post)]
-                                                            [:a] (enlive/set-attr :href (format "/%s/%s.html"
-                                                                                                (-> conf :dir :post)
-                                                                                                (:title post)))
-                                                            [:a :span.title] (enlive/content (:title post))
-                                                            [:a :span.date] (enlive/content (:date post))
-                                                            [:a :span.content] (enlive/html-content (:content post))))
-
-(enlive/defsnippet snippet-head
-  (io/file (:wd conf) (-> conf :dir :snippet) "head.html")
-  [:head :*]
-  [& {:keys [:meta] :as h}]
-  [:title] (enlive/content (:title (:meta h))))
+(defmacro find-snippet-files
+  "Return a list of files, or one file with given name."
+  [& {:keys [name] :as hm}]
+  (let [ext ".html"
+        pat #".*\.html$"
+        w (:wd conf)
+        d (-> conf :dir :snippet)
+        wd (remove nil? [w d])
+        n (:name hm)]
+    (cond
+      n `(let [f# (io/file ~@wd (str ~n ~ext))
+               r# (-> f# .getPath io/resource)]
+           ;; find snippet under working dir either resources dir
+           (if (fs/file? f#) f# r#))
+      :else `(-> (io/file ~@wd) .getPath (fs/find-files ~pat)))))
 
 (defn create-snippet-function
   [name file]
@@ -41,13 +39,27 @@
      (enlive/snippet file
                      [#{:head :body} :> :*]
                      [& args]))))
+(def snippet-post-list
+  (enlive/snippet
+   (find-snippet-files :name "post-list")
+   [#{:ul :ol}]
+   [& args]
+   [#{:ul :ol} [:li enlive/first-of-type]]
+   (enlive/clone-for [post (list-post)]
+                     [:a] (enlive/set-attr :href
+                                           (format "/%s/%s.html"
+                                                   (-> conf :dir :post)
+                                                   (:title post)))
+                     [:a :span.title] (enlive/content (:title post))
+                     [:a :span.date] (enlive/content (:date post))
+                     [:a :span.content] (enlive/html-content (:content post)))))
 
-(defn find-snippet-files
-  []
-  (let [file (io/file (:wd conf) (-> conf :dir :snippet))
-        path (.getPath file)
-        pat #".*\.html$"]
-    (fs/find-files path pat)))
+(def snippet-head
+  (enlive/snippet
+   (find-snippet-files :name "head")
+   [:head :*]
+   [& {:keys [:meta] :as h}]
+   [:title] (enlive/content (:title (:meta h)))))
 
 (defstate snippet :start (doseq [f (find-snippet-files)]
                            (create-snippet-function (fs/base-name f true) f)))
